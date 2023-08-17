db8a61fd76524bf9530e309b78a1d59370443fde
MrDadzie <gdnddz@gmail.com>
2023-08-17 15:39:01 -0400

    api.py running

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ import os, uvicorn
 from typing import List, Literal
 from pydantic import BaseModel
 import joblib
+from fastapi.encoders import jsonable_encoder
 
 
 #CONFIG
@@ -27,17 +28,33 @@ class Input(BaseModel):
     
 
 
-#ENDPOI
+#ENDPOINT
 @app.get("/Status")
 async def root():
     return {"message": "Online"}
 
 @app.post("/predict/")
 def predict(input: Input):
-    model = joblib.load("path_to_your_model.pkl")
-    features = [input.Plasma_Glucose, input.Blood_Work_Result1, input.Blood_Pressure,input.Blood_Work_Result2,input.Blood_Work_Result2]
-    prediction = model.predict([features])[0]
-    return {"prediction": prediction
+    scaler = joblib.load("Assets/scaler.joblib")
+    model = joblib.load("Assets/model.joblib")
+    
+    features = [input.Plasma_Glucose, 
+input.Blood_Work_Result1, 
+input.Blood_Pressure,
+input.Blood_Work_Result2,
+input.Blood_Work_Result3,
+input.Body_Mass_Index,
+input.Blood_Work_Result4,
+input.Age,input.Insurance]
+
+
+    scaled_features = scaler.transform([features])[0]
+    prediction = model.predict([scaled_features])[0]
+    
+    # Serialize the prediction result using jsonable_encoder
+    serialized_prediction = jsonable_encoder({"prediction": int(prediction)})
+    return serialized_prediction
+
 
 if __name__ == '__main__':
     uvicorn.run('api:app', reload =True)
